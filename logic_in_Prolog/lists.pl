b85d4676d405cbeb8c5e0d9675ae5b8e70005061
mzeuner <max@zeuner.net>
2023-02-20 18:04:59 +0100

    add from live session

--- logic_in_Prolog/lists.pl
+++ logic_in_Prolog/lists.pl
@@ -3,11 +3,14 @@
 member(X,[X|_]).
 member(X,[_|R]) :- member(X,R).
 
+%% counting occurrences of an element in a list
+count(X,XS,L) :- findall(X,member(X,XS),Bag),length(Bag,L).
+
+%% removing duplicates from the back, so each member only occurs once
 remove_duplicates([],[]).
 remove_duplicates([X|Y],Z) :- member(X,Y), remove_duplicates(Y,Z), !.
 remove_duplicates([X|Y],[X|Z]) :- remove_duplicates(Y,Z).
 
-%% the first two lists
 append([],X,X).
 append([X|Y],Z,[X|W]) :- append(Y,Z,W).
 
