7503f710042e22f03f7274dd2f13acdd3363d024
mzeuner <max@zeuner.net>
2024-02-12 18:14:24 +0100

    bubble sort

--- functional_in_Haskell/sorting.hs
+++ functional_in_Haskell/sorting.hs
@@ -1,6 +1,6 @@
 import Test.QuickCheck
 
--- First, we implement insertion sort
+-- insertion sort
 insertInSorted :: Ord a => a -> [a] -> [a]
 insertInSorted x [] = [ x ]
 insertInSorted x (y : xs) = if (x < y)
@@ -12,7 +12,7 @@ insertionSort [] = []
 insertionSort (x : xs) = insertInSorted x (insertionSort xs)
 
 
--- Second, an implementation of merge sort
+-- merge sort
 merge :: Ord a => [a] -> [a] -> [a]
 merge xs [] = xs
 merge [] ys = ys
@@ -30,9 +30,23 @@ mergeSort xs = let n = length xs `div` 2
                in merge (mergeSort ys) (mergeSort zs)
 
 
--- Let's test our two sorting functions
+-- bubble sort
+bubble :: Ord a => [a] -> [a]
+bubble [] = []
+bubble [x] = [x]
+bubble (x:y:xs) = if (x < y)
+                     then x: bubble (y:xs)
+                     else y: bubble (x:xs)
+
+bubbleSort :: Ord a => [a] -> [a]
+bubbleSort [] = []
+bubbleSort xs = let ys = bubble xs
+                in bubbleSort (init ys) ++ [last ys]
+
+
+-- Let's test our sorting functions
 testSorted :: [Int] -> Bool
-testSorted xs = (mergeSort xs == insertionSort xs)
+testSorted xs = (mergeSort xs == insertionSort xs) && (mergeSort xs == bubbleSort xs)
 
 main :: IO ()
 main = do
@@ -43,6 +57,9 @@ main = do
   putStrLn ("Sorting " ++ show mylist ++ " with merge sort gives")
   let y = mergeSort mylist
   putStrLn $ show y
+  putStrLn ("Sorting " ++ show mylist ++ " with bubble sort gives")
+  let z = bubbleSort mylist
+  putStrLn $ show z
 
   -- automated random testing
   quickCheck (withMaxSuccess 1000 testSorted)
